b08b3dd12f2685b8b522f4a4fedce39e6b9aef17
Ej Naranjo <ejvnaranjo@multisyscorp.com>
2023-03-30 20:15:10 +0800

    BBC NUMBAWAN!!!

--- Build/JNP-Build.framework.js
+++ Build/JNP-Build.framework.js
@@ -1984,13 +1984,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  4473152: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 4473207: function($0) {performance.now = function() { return $0; };},  
- 4473255: function($0) {performance.now = function() { return $0; };},  
- 4473303: function() {performance.now = Module['emscripten_get_now_backup'];},  
- 4473358: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 4473419: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 4473483: function() {return Module.webglContextAttributes.powerPreference;}
+  4472960: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 4473015: function($0) {performance.now = function() { return $0; };},  
+ 4473063: function($0) {performance.now = function() { return $0; };},  
+ 4473111: function() {performance.now = Module['emscripten_get_now_backup'];},  
+ 4473166: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 4473227: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 4473291: function() {return Module.webglContextAttributes.powerPreference;}
 };
 
 
